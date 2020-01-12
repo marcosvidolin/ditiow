9b3ccb14c8ab8c85d61c08687ff69b0b80582110
Marcos Vidolin <marcosvidolin@gmail.com>
2020-01-12 09:57:44 -0300

    Removing the constructor with the class arg and rename the conversor method to 'toDomain()'

--- src/main/java/com/vidolima/ditiow/resource/AbstractResource.java
+++ src/main/java/com/vidolima/ditiow/resource/AbstractResource.java
@@ -1,10 +1,11 @@
 package com.vidolima.ditiow.resource;
 
 import com.vidolima.ditiow.util.CopyPropertiesHelper;
+import java.lang.reflect.ParameterizedType;
 
 /**
  * Abstract resource class that defines the contract to convert a resource into
- * a business class.
+ * a domain class.
  *
  * @author Marcos A. Vidolin de Lima
  * @since Dez 23, 2019
@@ -12,19 +13,21 @@ import com.vidolima.ditiow.util.CopyPropertiesHelper;
  */
 public abstract class AbstractResource<B> {
 
-  private Class<B> businessClass;
+  private Class<B> domainClass;
 
-  public AbstractResource(Class<B> businessClass) {
-    this.businessClass = businessClass;
+  public AbstractResource() {
+    this.domainClass = ((Class<B>) ((ParameterizedType) getClass()
+        .getGenericSuperclass()).getActualTypeArguments()[0]);
   }
 
   /**
+   * Returns a converted object of the "B" type.
    *
-   * @param <B>
-   * @return
+   * @param <B> the type of domain class
+   * @return the object
    */
-  public <B> B toBusiness() {
-    return (B) CopyPropertiesHelper.copy(this, businessClass);
+  public <B> B toDomain() {
+    return (B) CopyPropertiesHelper.copy(this, domainClass);
   }
 
 }
